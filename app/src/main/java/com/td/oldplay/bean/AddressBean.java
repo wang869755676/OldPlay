c1483d7cc209baf9fd8f651a0b5ef8b6c14bdf0c
869755676@qq.com <869755676@qq.com>
2017-07-14 18:31:58 +0800

    ui图修改

--- app/src/main/java/com/td/oldplay/bean/AddressBean.java
+++ app/src/main/java/com/td/oldplay/bean/AddressBean.java
@@ -1,8 +1,10 @@
 package com.td.oldplay.bean;
 
+import java.io.Serializable;
+
 /**
  * Created by my on 2017/7/10.
  */
 
-public class AddressBean {
+public class AddressBean  implements Serializable{
 }
